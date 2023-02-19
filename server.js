e8b5faaee2b291d8b3d00b1e63a5028567749f3e
Ryebread5555 <Rypetty55@gmail.com>
2023-02-19 14:30:31 -0600

    imported, connected to database, create prompt

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 // TODO import and require mysql12 and inquirer
+const mysql = require('mysql12');
+const inquirer = require('inquirer');
 
 // connect to database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        databass: 'employee_db'
+    },
+);
+
+db.connect(err => {
+    if (err) throw err;
+    console.log(`Connected to employee_db database.`)
+   connectionComplete(); 
+});
+
+connectionComplete = () => {
+    console.log("+------------------------+")
+    console.log("|                        |")
+    console.log("|    Employee Manager    |")
+    console.log("|                        |")
+    console.log("+------------------------+")
+    promptUser();
+};
 
 // TODO prompt user
+
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+
+        }
+    ])
+};
