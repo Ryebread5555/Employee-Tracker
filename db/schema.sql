d31f8cf0734b449435d935a4f3db4f90e6b3169e
Ryebread5555 <Rypetty55@gmail.com>
2023-02-20 14:51:55 -0600

    fixed "," in wrong spot

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ USE employee_db;
 -- TODO create table for department --
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 -- TODO create table for role --
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
+    department_id INT
 );
 -- TODO create table for employee --
 CREATE TABLE employee (
@@ -21,5 +21,5 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
+    manager_id INT
 );
